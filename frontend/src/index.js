271946dfd134f650235415e0868694c174333064
baoooliang <73327355+baoooliang@users.noreply.github.com>
2021-03-02 07:55:47 -0800

    Add vercel config

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,17 +6,12 @@ import {Provider as ReduxProvider} from "react-redux";
 import "./components/common/assets/scss/material-dashboard-pro-react.scss?v=1.9.0";
 import App from "./components/App";
 import configureStore from "./redux/configureStore";
-import Match from "./components/match/Match"
 const store = configureStore();
-
-/*<Router>
-      <App/>
-    </Router>*/
 
 render(
   <ReduxProvider store={store}>
     <Router>
-      <Match/>
+      <App/>
     </Router>
   </ReduxProvider>,
   document.getElementById("app")
